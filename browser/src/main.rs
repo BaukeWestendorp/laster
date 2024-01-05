1fe0f96fde189b8283e331faae12ad48938ccc6d
Bauke Westendorp <baukewestendorp@gmail.com>
2024-01-05 20:59:47 +0100

    Use `nannou` instead of `winit` for rendering `stammer` panel

--- browser/src/main.rs
+++ browser/src/main.rs
@@ -1,33 +1,16 @@
 #![feature(array_chunks)]
 
+use nannou::{image, prelude::*};
+
 use std::io::Read;
 use std::rc::Rc;
 
 use dom::arena::NodeArena;
 use dom::node::{Node, NodeKind};
 use fleck::Font;
-use pixels::wgpu::BlendState;
-use pixels::{PixelsBuilder, SurfaceTexture};
 use stammer::elements::builder::ElementBuilder;
 use stammer::elements::{Element, SizingStrategy};
 use stammer::Panel;
-use winit::dpi::{LogicalSize, PhysicalSize};
-use winit::event::Event;
-use winit::event_loop::EventLoop;
-use winit::window::{Window, WindowBuilder};
-use winit_input_helper::WinitInputHelper;
-
-const WINDOW_NAME: &str = env!("CARGO_BIN_NAME");
-
-fn setup_window(min_size: PhysicalSize<u32>, event_loop: &EventLoop<()>) -> Window {
-    let builder = WindowBuilder::new()
-        .with_transparent(true)
-        .with_resizable(true)
-        .with_title(WINDOW_NAME)
-        .with_inner_size(min_size)
-        .with_min_inner_size(min_size);
-    builder.build(&event_loop).expect("should build window")
-}
 
 fn load_font(path: &str) -> std::io::Result<Font> {
     let mut file = std::fs::File::open(path)?;
@@ -72,124 +55,6 @@ struct Data {
     height: u32,
 }
 
-fn main() -> Result<(), pixels::Error> {
-    let mut arena = dom::arena::NodeArena::new();
-    let document = get_document(&mut arena);
-    let body = get_body(&mut arena, &document);
-
-    let font_path = "/etc/tid/fonts/geneva14.uf2".to_string();
-    let font = match load_font(&font_path) {
-        Ok(font) => font,
-        Err(err) => {
-            eprintln!("ERROR: Failed to load font from {font_path:?}: {err}");
-            std::process::exit(1);
-        }
-    };
-
-    let event_loop = EventLoop::new();
-
-    let scale_factor = std::env::var("TID_SCALE_FACTOR")
-        .ok()
-        .and_then(|v| v.parse::<f32>().ok())
-        .map(|v| v.round() as u32)
-        .unwrap_or(1);
-
-    let mut document_element = dom_node_as_stammer_element(Rc::new(font), &body, &mut arena);
-    document_element.size.maxwidth = Some(600);
-    document_element.size.maxheight = Some(400);
-
-    let data = Data {
-        width: 0,
-        height: 0,
-    };
-
-    let mut state = Panel::new(
-        document_element,
-        [0x00, 0x00, 0x00, 0xff],
-        [0xff, 0xff, 0xff, 0xff],
-        data,
-    );
-
-    let (width, height) = (state.width, state.height);
-    // TODO: This is _SUCH_ a papercut or even pitfall, as I just saw.
-    state.data_mut().width = width;
-    state.data_mut().height = height;
-    let size = PhysicalSize::new(width * scale_factor, height * scale_factor);
-
-    let mut input = WinitInputHelper::new();
-    let window = setup_window(size, &event_loop);
-
-    let mut pixels = {
-        let window_size = window.inner_size();
-        let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
-        PixelsBuilder::new(width, height, surface_texture)
-            .clear_color({
-                let [r, g, b, a] = state.background.map(|v| v as f64 / u8::MAX as f64);
-                pixels::wgpu::Color { r, g, b, a }
-            })
-            .blend_state(BlendState::REPLACE) // TODO: Investigate rendering weirdness.
-            .build()?
-    };
-
-    event_loop.run(move |event, _, control_flow| {
-        control_flow.set_poll();
-
-        match event {
-            // Event::NewEvents(winit::event::StartCause::ResumeTimeReached { .. }) => {
-            //     window.request_redraw()
-            // }
-            Event::RedrawRequested(_) => {
-                // Clear the screen before drawing.
-                pixels
-                    .frame_mut()
-                    .array_chunks_mut()
-                    .for_each(|px| *px = state.background);
-
-                // Update the state, then draw.
-                state.update();
-                state.draw(&mut pixels.frame_mut());
-
-                // Try to render.
-                if let Err(err) = pixels.render() {
-                    eprintln!("ERROR: {err}");
-                    control_flow.set_exit();
-                    return;
-                }
-            }
-            _ => (),
-        }
-
-        if input.update(&event) {
-            // Close events.
-            if input.close_requested() {
-                eprintln!("INFO:  Close requested. Bye :)");
-                control_flow.set_exit();
-                return;
-            }
-
-            // Resize the window.
-            if let Some(size) = input.window_resized() {
-                eprintln!("INFO:  Resize request {size:?}");
-                let ps = PhysicalSize {
-                    width: (size.width / scale_factor) * scale_factor,
-                    height: (size.height / scale_factor) * scale_factor,
-                };
-                let ls = LogicalSize {
-                    width: ps.width / scale_factor,
-                    height: ps.height / scale_factor,
-                };
-                pixels.resize_surface(ps.width, ps.height).unwrap();
-                pixels.resize_buffer(ls.width, ls.height).unwrap();
-                window.set_inner_size(ps);
-                state.resize(ls.width, ls.height);
-                state.data_mut().width = ls.width;
-                state.data_mut().height = ls.height;
-                window.request_redraw();
-            }
-        }
-    });
-}
-
 fn get_document(arena: &mut NodeArena) -> Node {
     let mut html = String::new();
     std::io::stdin().read_to_string(&mut html).unwrap();
@@ -207,4 +72,85 @@ fn get_body(arena: &mut NodeArena, document: &Node) -> Node {
     let body = arena.get_node(head).children()[7];
     let body = arena.get_node(body).clone();
     body
+}
+
+fn main() {
+    nannou::app(model)
+        .event(event)
+        .loop_mode(LoopMode::Wait)
+        .run();
+}
+
+struct Model {
+    texture: wgpu::Texture,
+}
+
+fn model(app: &App) -> Model {
+    let mut arena = dom::arena::NodeArena::new();
+    let document = get_document(&mut arena);
+    let body = get_body(&mut arena, &document);
+
+    let font_path = "/etc/tid/fonts/times15.uf2".to_string();
+    let font = match load_font(&font_path) {
+        Ok(font) => font,
+        Err(err) => {
+            eprintln!("ERROR: Failed to load font from {font_path:?}: {err}");
+            std::process::exit(1);
+        }
+    };
+
+    let mut document_element = dom_node_as_stammer_element(Rc::new(font), &body, &mut arena);
+    document_element.size.maxwidth = Some(512);
+    document_element.size.maxheight = Some(512);
+
+    let data = Data {
+        width: 0,
+        height: 0,
+    };
+
+    let mut state = Panel::new(
+        document_element,
+        [0x00, 0x00, 0x00, 0xff],
+        [0xff, 0xff, 0xff, 0xff],
+        data,
+    );
+
+    let (width, height) = (state.width, state.height);
+    state.data_mut().width = width;
+    state.data_mut().height = height;
+
+    let window = app.new_window().size(512, 512).view(view).build().unwrap();
+    let window = app.window(window).unwrap();
+
+    let mut pixels = [state.background; 512 * 512].concat().to_vec();
+
+    state.draw(&mut pixels);
+
+    let buffer = image::ImageBuffer::from_fn(512, 512, |x, y| {
+        let index = (y * 512 + x) as usize;
+        let pixel = [
+            pixels[index * 4],
+            pixels[index * 4 + 1],
+            pixels[index * 4 + 2],
+            pixels[index * 4 + 3],
+        ];
+        image::Rgba(pixel)
+    });
+
+    let texture = wgpu::Texture::load_from_image_buffer(
+        window.device(),
+        window.queue(),
+        wgpu::TextureUsages::TEXTURE_BINDING,
+        &buffer,
+    );
+
+    Model { texture }
+}
+
+fn event(_app: &App, _model: &mut Model, _event: Event) {}
+
+fn view(app: &App, model: &Model, frame: Frame) {
+    let draw = app.draw();
+    draw.texture(&model.texture);
+    draw.to_frame(app, &frame).unwrap();
 }
